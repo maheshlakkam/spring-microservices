64c8dfcab2209cfee8badd863bbfd0dde3c56d6c
Mahesh Lakkam <mlakkam@evoketechnologies.com>
2018-04-26 14:55:03 +0530

    Adding Files

---
+++ src/main/groovy/com/test/Springmicroservices/SpringMicroservicesApplication.groovy
@@ -0,0 +1,12 @@
+package com.test.Springmicroservices
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringMicroservicesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringMicroservicesApplication, args
+	}
+}
